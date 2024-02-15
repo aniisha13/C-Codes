7ce98a86cabe1c3de2ce78dbaacd0818c8443bef
Anisha Rathore <115221306+aniisha13@users.noreply.github.com>
2024-02-15 23:36:57 +0530

    convert the temp.cpp

--- convert the temp.cpp
+++ convert the temp.cpp
@@ -3,8 +3,11 @@
 #include<vector> 
 using namespace std;
 
+//we have to convert the temperature in celsius to kelvin and Fahrenheit
 class Solution {
 public:
+/*solution: let's create a dynamic array(vector) of datatype double named convertTemperature taking one parameter celsius and store the output(converted values) 
+ in another dynamic array named output , now to store the values in vector array we use push_back member function and return the output array*/
     vector<double> convertTemperature(double celsius) {
         vector<double> output;
         double kelvin = celsius+ 273.15;
@@ -21,11 +24,11 @@ int main()
 {
 	double celsius;
 	cout<<"Enter temperature in Celsius: ";
-    cin>>celsius;
+        cin>>celsius;
 	Solution s;
 	s.convertTemperature(celsius);
 	vector<double> result = s.convertTemperature(celsius);
 	cout<<"Temperature in Kelvin: "<<result[0]<<endl;
-    cout<<"Temperature in Fahrenheit: "<<result[1]<<endl;
+        cout<<"Temperature in Fahrenheit: "<<result[1]<<endl;
 	return 0;
 }
